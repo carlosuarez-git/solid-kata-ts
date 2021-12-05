50530c2304d9a59aee58834df53da93d40e799a6
José Carlos Calderón Suárez <jose.calderon@scalefast.com>
2021-12-05 13:28:24 +0100

    Test folder: srp

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)']
+  // testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)']
+  testMatch: ['**/srp/__tests__/**/*.spec.[jt]s?(x)']
 };
+
